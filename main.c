18f5f4a45b4568b825357dca81e626fe38b65077
Alex Norman <x37v.alex@gmail.com>
2011-04-26 23:37:34 -0700

    defining i/o

--- main.c
+++ main.c
@@ -5,9 +5,20 @@ void fallthrough_callback(MidiDevice * device, uint16_t cnt, uint8_t byte0, uint
    midi_send_data(device, cnt, byte0, byte1, byte2);
 }
 
+
 int main(void) {
    MidiDevice usb_midi;
    midi_usb_init(&usb_midi);
+
+   //e0 serial output
+   DDRE |= _BV(PE0);
+
+	//porta is all inputs with pullups
+	DDRA = 0;
+	PORTA = 0xFF;
+
+   //portb all outputs
+   DDRB = 0xFF;
 
    midi_register_fallthrough_callback(&usb_midi, fallthrough_callback);
 
