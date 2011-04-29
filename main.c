3c5e1c6c8195e45622fc3e77953ccbdf9de35011
Alex Norman <x37v.alex@gmail.com>
2011-04-28 22:09:28 -0700

    got the 2nd led row to light up

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "midi_usb.h"
-#include "avr/io.h"
+#include <avr/io.h>
+#include <util/delay.h>
 
 void fallthrough_callback(MidiDevice * device, uint16_t cnt, uint8_t byte0, uint8_t byte1, uint8_t byte2){
    midi_send_data(device, cnt, byte0, byte1, byte2);
@@ -15,13 +16,13 @@ void fallthrough_callback(MidiDevice * device, uint16_t cnt, uint8_t byte0, uint
  * b5 u7 reset
  * b4 u7 clock
  *
- * led power [columns]:
+ * led power [row]:
  * b3 u5 clock
  * b2 u5 reset
  *
- * led grounds [rows]:
- * b1 u3 SH_CP
- * b0 u3 ST_CP
+ * led grounds [column]:
+ * b1 u3 SH_CP [data reg, serial in, pos edge]
+ * b0 u3 ST_CP [storage reg, parallel out, pos edge]
  *
  * e0 shared serial out
  *
@@ -29,8 +30,35 @@ void fallthrough_callback(MidiDevice * device, uint16_t cnt, uint8_t byte0, uint
  * porta
  */
 
+#define SH_CP PB1
+#define ST_CP PB0
+
+#define LED_POWER_CLK PB3
+#define LED_POWER_RST PB2
+
+volatile uint8_t cur_led_row;
+
+//set the serial out value to the LS bit of v
+void setSerial(uint8_t v) {
+   //set pin e0
+   PORTE = (PINE & ~0x1) | (v & 0x1);
+   _delay_us(10);
+}
+
+void shiftLedCol(uint8_t col) {
+   uint8_t i;
+   PORTB &= ~_BV(ST_CP);
+   for(i = 0; i < 8; i++) {
+      PORTB &= ~_BV(SH_CP);
+      setSerial(col >> i);
+      PORTB |= _BV(SH_CP);
+   }
+   PORTB |= _BV(ST_CP);
+}
+
 
 int main(void) {
+   uint8_t i;
    MidiDevice usb_midi;
    midi_usb_init(&usb_midi);
 
@@ -44,9 +72,26 @@ int main(void) {
    //portb all outputs
    DDRB = 0xFF;
 
+   PORTB |= _BV(LED_POWER_RST);
+
    midi_register_fallthrough_callback(&usb_midi, fallthrough_callback);
 
+   PORTB &= ~_BV(LED_POWER_CLK);
+   setSerial(0);
+   PORTB |= _BV(LED_POWER_CLK);
+
+   PORTB &= ~_BV(LED_POWER_CLK);
+   setSerial(1);
+   PORTB |= _BV(LED_POWER_CLK);
+
+   for(i = 1; i < 7; i++) {
+      PORTB &= ~_BV(LED_POWER_CLK);
+      setSerial(0);
+      PORTB |= _BV(LED_POWER_CLK);
+   }
+
    while(1){
+      shiftLedCol(0xf0);
       midi_device_process(&usb_midi);
    }
 
